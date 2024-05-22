6a958fef373c3f49f0d3f4420031c8ad00cb553c
Areez <31994090+azer0ne@users.noreply.github.com>
2024-05-22 15:08:52 +0700

    Completed project
    Completed project of BMI Calculator App Brewery Course

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Abiyyu Firmansyah on 16/05/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    var bmiValue = "0.0"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+        
+        let label = UILabel()
+        label.text = bmiValue
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
+    }
+}
